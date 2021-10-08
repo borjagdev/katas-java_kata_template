d3be9d3db818303feb9cad796b3e7bd8b9bf5f19
Borja <borjagarcia.dev@gmail.com>
2021-10-08 12:02:10 +0100

    Initial project structure

---
+++ src/test/java/kata/DemoKataShould.java
@@ -0,0 +1,12 @@
+package kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemoKataShould {
+    @Test
+    void emptyTest() {
+        assertNull(null);
+    }
+}
